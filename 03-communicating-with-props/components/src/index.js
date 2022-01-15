a02e351ab598b2085f9059153a68f6df2c7fbd4e
ttolson-code <ttolson@protonmail.com>
2022-01-14 20:40:42 -0500

    Finished section 03: Communicating with Props.

---
+++ 03-communicating-with-props/components/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from '@faker-js/faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam" 
+          timeAgo="Today at 4:45PM" 
+          content="Nice blog post!" 
+          avatar={faker.image.image()} 
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Alex" 
+          timeAgo="Today at 2:00PM" 
+          content="Very cool." 
+          avatar={faker.image.image()} 
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Jane" 
+          timeAgo="Yesterday at 5:00PM" 
+          content="Nice blog post!" 
+          avatar={faker.image.image()} 
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
