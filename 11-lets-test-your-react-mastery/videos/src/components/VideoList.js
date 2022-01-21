4fe4287af51b271a3e1b2160b5d1d7e2901b3132
ttolson-code <ttolson@protonmail.com>
2022-01-20 23:28:08 -0500

    Finished section 11: Let's Test Your React Mastery!

--- 11-lets-test-your-react-mastery/videos/src/components/VideoList.js
+++ 11-lets-test-your-react-mastery/videos/src/components/VideoList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderedVideoList = videos.map((video) => {
-    return <VideoItem video={video} />;
+    return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} />;
   });
 
   return <div className="ui relaxed divided list">{renderedVideoList}</div>;
