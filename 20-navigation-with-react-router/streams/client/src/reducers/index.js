67e861987f32fc8ab6f27ef9e5ec56c23c2f7620
ttolson-code <ttolson@protonmail.com>
2022-02-27 22:48:23 -0500

    Added and configured redux-form library.

--- 20-navigation-with-react-router/streams/client/src/reducers/index.js
+++ 20-navigation-with-react-router/streams/client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+// Rename reducer variable from redux-form to avoid confusion
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
